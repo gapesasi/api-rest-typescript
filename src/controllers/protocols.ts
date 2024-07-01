32a3e5944fc12efff3621a44c377a5a73c0bb9e6
Gabriel Santana <95727078+gapesasi@users.noreply.github.com>
2024-07-01 18:46:01 -0300

    feat: create protocols interfaces

---
+++ src/controllers/protocols.ts
@@ -0,0 +1,20 @@
+export interface HttpResponse<T> {
+  statusCode: HttpStatusCode;
+  body: T;
+}
+
+export interface HttpRequest<B> {
+  userId?: number;
+  params?: any;
+  query?: any;
+  headers?: any;
+  body?: B;
+}
+
+export enum HttpStatusCode {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  UNPROCESSABLE_ENTITY = 422,
+  SERVER_ERROR = 500,
+}
