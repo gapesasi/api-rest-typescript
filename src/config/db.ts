178d15cc7cc4914e477929a2694208b23d50104d
Gabriel Santana <95727078+gapesasi@users.noreply.github.com>
2024-07-01 12:40:15 -0300

    feat: create db config

---
+++ src/config/db.ts
@@ -0,0 +1,10 @@
+import { ConnectionOptions } from "mysql2";
+
+const config: ConnectionOptions = {
+  host: "localhost",
+  user: "root",
+  password: "docker",
+  database: "api_typescript",
+};
+
+export default config;
