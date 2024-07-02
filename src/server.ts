b44a12ed33d2f99c5f9063636386716a30bfe590
Gabriel Santana <95727078+gapesasi@users.noreply.github.com>
2024-07-02 10:35:30 -0300

    feat: add book routes to the app

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { createServer, Server as HttpServer } from "http";
+import { router } from "./routes";
 
 export default class Server {
   public app: express.Application;
@@ -8,15 +9,22 @@ export default class Server {
   constructor() {
     this.app = express();
     this.server = createServer(this.app);
+    this.setupMiddlewares();
     this.setupRoutes();
+  }
+
+  private setupMiddlewares() {
+    this.app.use(express.json());
   }
 
   private setupRoutes() {
     this.app.get("/test", (req, res) => {
-        res.status(200).json({
-            message: "OK"
-        })
+      res.status(200).json({
+        message: "OK",
+      });
     });
+
+    this.app.use("/", router);
   }
 
   public start(port: number) {
